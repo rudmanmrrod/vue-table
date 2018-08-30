d7545db47d7e98f330f6e4a359de9229f712d495
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-08-30 14:54:48 -0400

    Edited home

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <h1>Welcome to vue datatable</h1>
+    <h1>Welcome to vue table</h1>
+    <p>You can view your heroes favorites from marvel</p>
+    Powered by <a href="https://developer.marvel.com" target="_blank">Marvel Developer Api</a>
   </div>
 </template>
 
