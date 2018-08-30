04d21198b6abe987848664a985376a7d9f91c1f5
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-08-30 11:18:31 -0400

    Added dynamic pagination and items per page

--- src/components/Datatable/DatatableComponent.js
+++ src/components/Datatable/DatatableComponent.js
@@ -9,15 +9,22 @@ export default {
     		{label:'Name',field:'name'},
     		{label:'Description',field:'description'}
     	],
-    	rows:[]
+    	rows:[],
+      total:0,
+      limit:10,
+      offset:0,
+      pageDropdown:"[10,20]",
     }
   },  
   methods: {
   	getChars(){
+      console.log(this.offset)
   	   let data = {
         'apikey':'6b46e44334a56db24631af8033d776be',
         'ts':'1',
-        'hash':'d916d187a1c11858352d75c90ca19823'
+        'hash':'d916d187a1c11858352d75c90ca19823',
+        'limit':this.limit,
+        'offset':this.offset
       };
       axios
       .get('https://gateway.marvel.com/v1/public/characters', {
@@ -25,22 +32,57 @@ export default {
       })
       .then(response => {
       	let data = response.data.data;
-        data.total
-        data.count
+        this.total = data.total/data.count;;
         for (let hero of data.results) {
         	let row = {
         		image:hero.thumbnail.path+'.'+hero.thumbnail.extension,
         		name:hero.name,
         		description:hero.description
         	};
-        	console.log(row.image)
-            this.rows.push(row)
+          this.rows.push(row)
         }
       })
       .catch(error => {
         console.log(error.response);
       });
-  	}
+  	},
+    onPageChange(params) {
+      console.log(params)
+      if(params.currentPage > params.prevPage){
+        this.offset += this.limit;
+      }
+      else{
+        if(this.offset > 0){
+          this.offset -= this.limit;
+        }
+      }
+      this.rows = [];
+      this.getChars();
+      //this.updateParams({page: params.currentPage});
+      //this.loadItems();
+    },
+
+    onPerPageChange(params) {
+      this.limit = params.currentPerPage
+      this.rows = [];
+      this.getChars();
+      //this.updateParams({perPage: params.currentPerPage});
+      //this.loadItems();
+    },
+
+    onSortChange(params) {
+      console.log(params)
+      /*this.updateParams({
+        sort: {
+          type: params.sortType,
+          field: this.columns[params.columnIndex].field,
+        },
+      });*/
+    },
+    
+    onColumnFilter(params) {
+      console.log(params)
+    }
   },
   mounted(){
   	this.getChars();
--- src/components/Datatable/DatatableTemplate.vue
+++ src/components/Datatable/DatatableTemplate.vue
@@ -1,6 +1,19 @@
 <template>
 	<div>
-		<vue-good-table :columns="columns" :rows="rows">
+		<vue-good-table 
+			:columns="columns" 
+			:rows="rows" 
+			:pagination-options="{
+				enabled: true,
+				perPageDropdown: [10,20],
+    			dropdownAllowAll: false,
+			}"
+			mode="remote"
+			@on-page-change="onPageChange"
+			@on-sort-change="onSortChange"
+			@on-column-filter="onColumnFilter"
+			@on-per-page-change="onPerPageChange"
+			:totalRows="total">
 			<template slot="table-row" slot-scope="props">
 			    <span v-if="props.column.field == 'image'">
 			      <img v-bind:src=" props.row.image" width="150px"/> 
